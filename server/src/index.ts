3f0c4d97e64a472f43335fe88113595b401eec46
Victor MARFIL <vicmarvu98@gmail.com>
2025-03-08 14:18:11 +0100

    Create Dockerfile

--- server/src/index.ts
+++ server/src/index.ts
@@ -69,7 +69,7 @@ async function bootstrap() {
   });
 
   try {
-    await app.listen({ port: env.PORT });
+    await app.listen({ port: env.PORT, host: '0.0.0.0' });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
